3585032d8c7274e2f2fa64d2f36ecb97635db3fb
Akshay-Apex <127332144+Akshay-Apex@users.noreply.github.com>
2024-02-29 20:47:19 +0530

    Add files via upload

--- ChargeTimeCalc.java
+++ ChargeTimeCalc.java
@@ -6,18 +6,37 @@
 
 package ChargeTimeCalculator;
 import java.util.Scanner;
-
-public class ChargeTimeCalc {			
-	public static void main(String[] args) {						
+ 
+public class ChargeTimeCalc {				
+	public static void main(String[] args) {	
 		Scanner scan = new Scanner(System.in);
-		byte currentChargePer, charge15Min, chargePerReq, offset;
-		currentChargePer = charge15Min = chargePerReq = offset = 0;
+		byte currentChargePer, charge15Min, chargePerReq, offset, negOffset;
+		currentChargePer = charge15Min = chargePerReq = offset = negOffset = 0;		
 		int hours, minutes, seconds;		
 		hours = minutes = seconds = 0;
-		float total = 0.0f;
-		String scanReq = "";
+		float total = 0.0f, time = 0.0f;		
+		String scanReq = "", calcMode = "";
 				
-		while (true) {
+		while (true) {								
+			if (calcMode == "") {				
+				System.out.print("Choose the \"Calculation\" mode, what do you want percentage or time [p, t]: ");
+				calcMode = scan.nextLine();				
+				if (calcMode.equals("p") || calcMode.equals("P")) {
+					calcMode = "percentage";
+				} else if (calcMode.equals("t") || calcMode.equals("T")) {
+					calcMode = "time";
+				}
+			} 
+											
+			//Error handling for choosingCalcMode
+			if (!calcMode.equals("percentage") == !calcMode.equals("time")) {
+				System.out.println("\n+-------------WARNING!-------------+\n"
+								   + "| Please enter the given keyword!  |\n"
+								   + "+----------------------------------+\n");				
+				calcMode = "";
+				continue;
+			}
+			
 			switch (scanReq) {
 				case "currentChargePer":					
 					System.out.print("Enter the remaining charge percentage: ");
@@ -35,80 +54,177 @@ public class ChargeTimeCalc {
 					System.out.print("Enter the offset percentage: ");
 					offset = scan.nextByte();				
 					break;
+				case "negOffset":					
+					System.out.print("Enter the negative offset percentage: ");
+					negOffset = scan.nextByte();				
+					break;
+				case "hours":										
+					System.out.print("Enter hours: ");
+					hours = scan.nextInt();
+					break;
+				case "minutes":
+					System.out.print("Enter minutes: ");
+					minutes = scan.nextInt();
+					break;
+				case "seconds":
+					System.out.print("Enter seconds: ");
+					seconds = scan.nextInt();				
+					break;
 				default:
 					System.out.print("Enter the remaining charge percentage: ");
 					currentChargePer = scan.nextByte();
 					System.out.print("Enter the percentage your device can charge upto in 15 min: ");
 					charge15Min = scan.nextByte();
-					System.out.print("Enter the maximum charge percentage you require: ");
-					chargePerReq = scan.nextByte();	
-					System.out.print("Enter the offset percentage: ");
-					offset = scan.nextByte();										
-			}
-								
+					
+					if (calcMode.equals("time")) {
+						System.out.print("Enter the maximum charge percentage you require: ");
+						chargePerReq = scan.nextByte();		
+						System.out.print("Enter the offset percentage: ");
+						offset = scan.nextByte();	
+					} else if (calcMode.equals("percentage")) {						
+						System.out.println("Enter the maximum time you have to charge your device: ");
+						System.out.print("Enter hours: ");
+						hours = scan.nextInt();
+						System.out.print("Enter minutes: ");
+						minutes = scan.nextInt();
+						System.out.print("Enter seconds: ");
+						seconds = scan.nextInt();					
+					}																	
+			}			
+			
+			
 			//Checks if the entered values are correct 
-			if (currentChargePer < 1) {
-				System.out.println("+--------------ERROR!--------------+\n"
-								 + "| The remaining charge percentage  |\n"
-								 + "| cant't be less than 1 (one)!     |\n"							 
-								 + "+----------------------------------+");
+			if (currentChargePer < 0 || currentChargePer > 100) {
+				
+				if (currentChargePer > 100) {
+					System.out.println("\n+--------------ERROR!--------------+\n"
+									   + "| The remaining charge percentage  |\n"
+									   + "| cant't be greater than 100       |\n"
+									   + "| (hundred) !                      |\n"							 
+									   + "+----------------------------------+\n");
+					scanReq = "currentChargePer";
+					continue;
+				}
+				
+				System.out.println("\n+--------------ERROR!--------------+\n"
+								   + "| The remaining charge percentage  |\n"
+								   + "| cant't be less than 0 (zero)!    |\n"							 
+								   + "+----------------------------------+\n");
 				scanReq = "currentChargePer";
 				continue;
 			} else if (charge15Min < 1) {
-				System.out.println("+--------------ERROR!--------------+\n"
-								 + "| The percentage your device can   |\n"
-								 + "| charge upto in 15 min must be    |\n"
-								 + "| greater than 0 (zero)!           |\n"
-								 + "+----------------------------------+");
+				System.out.println("\n+--------------ERROR!--------------+\n"
+								   + "| The percentage your device can   |\n"
+								   + "| charge upto in 15 min must be    |\n"
+								   + "| greater than 0 (zero)!           |\n"
+								   + "+----------------------------------+\n");
 				scanReq = "charge15Min";
 				continue;
-			} else if (chargePerReq < currentChargePer) {										
-				
-				//Fixes the end String depending on the currentChargePer digit count
-				String endStrFix;
-				if (currentChargePer < 10) {					
-					endStrFix = "      |\n";
-				} else if (currentChargePer < 100) {					
-					endStrFix = "     |\n";
-				} else {					
-					endStrFix = "    |\n";
+			} 
+			
+			
+			if (calcMode.equals("time")) {
+				//Checks if the entered values are correct 
+				if (chargePerReq < currentChargePer || chargePerReq > 100) {	
+					
+					if(chargePerReq > 100) {
+						System.out.println("\n+--------------ERROR!--------------+\n"
+										   + "| The charge percentage required   |\n"
+										   + "| can't be greater than 100        |\n"
+										   + "| (hundred)!                       |\n"
+										   + "+----------------------------------+\n");
+						scanReq = "chargePerReq";
+						continue;
+					}
+										
+					//Fixes the end String depending on the currentChargePer digit count
+					String endStrFix;
+					if (currentChargePer < 10) {					
+						endStrFix = "      |\n";
+					} else if (currentChargePer < 100) {					
+						endStrFix = "     |\n";
+					} else {					
+						endStrFix = "    |\n";
+					}
+					
+					System.out.println("\n+---------------ERROR!---------------+\n"
+									   + "| The charge percentage you require  |\n"
+	  				  				   + "| can't be less than the remaining   |\n"
+	  				  				   + "| charge percentage which was " + currentChargePer + endStrFix
+									   + "+------------------------------------+\n");
+					scanReq = "chargePerReq";
+					continue;
+				} else if (offset < 0) {
+					System.out.println("\n+--------------ERROR!--------------+\n"
+									   + "| The offset value can't be less   |\n"
+									   + "| than 0 (zero)!                   |\n"
+									   + "+----------------------------------+\n");
+					scanReq = "offset";
+					continue;
 				}
 				
-				System.out.println("+---------------ERROR!---------------+\n"
-								 + "| The charge percentage you require  |\n"
-  				  				 + "| can't be less than the remaining   |\n"
-  				  				 + "| charge percentage which was " + currentChargePer + endStrFix
-								 + "+------------------------------------+");
-				scanReq = "chargePerReq";
-				continue;
-			} else if (offset < 0) {
-				System.out.println("+--------------ERROR!--------------+\n"
-								 + "| The offset value can't be less   |\n"
-								 + "| than 0 (zero)!                   |\n"
-								 + "+----------------------------------+");
-				scanReq = "offset";
-				continue;
+			} else if (calcMode.equals("percentage")) {
+				//Checks if the hours, minutes, and seconds are correct
+				if (hours < 0) {
+					System.out.println("\n+--------------ERROR!--------------+\n"
+									   + "| The hours value can't be less    |\n"
+									   + "| than 0 (zero)!                   |\n"							 
+									   + "+----------------------------------+\n");
+					scanReq = "hours";
+					continue;
+				} else if (minutes < 0) {
+					
+					System.out.println("\n+--------------ERROR!--------------+\n"
+									   + "| The minutes value can't be less  |\n"
+									   + "| than 0 (zero) !                  |\n"									   							
+									   + "+----------------------------------+\n");
+					scanReq = "minutes";
+					continue;
+				} else if (seconds < 0) {
+					System.out.println("\n+--------------ERROR!--------------+\n"
+									   + "| The seconds value can't be less  |\n"
+									   + "| than 0 (zero) !                  |\n"									   							
+									   + "+----------------------------------+\n");
+					scanReq = "seconds";
+					continue;
+				} 
+			}								
+			
+			//This code calculates the time or percentage depending on the calcMode
+			if (calcMode.equals("time")) {
+				//Calculating the Total Time in Minutes 
+				total = (( (float) (chargePerReq + offset) - currentChargePer) / charge15Min) * 15;								
+				total = Float.parseFloat(String.format("%.2f", total));			
+				
+				//Converting the total time into hours, minutes, and seconds 
+				hours = (int) total / 60;			
+				minutes = (int) total - (hours * 60);			
+				seconds = (int) ((total - (hours * 60) - minutes) * 60);								
+				System.out.println("The Approximate time required to charge the device is: " 
+						    	   + hours + ":" + minutes + "'" + seconds + "\"");							
+			} else if (calcMode.equals("percentage")) {																
+				//Converts time in minutes to percentage 
+				time = (hours * 60) + minutes + (seconds / 60.0f);						
+				time = ((time / 15.0f) * charge15Min) + currentChargePer;	
+				time = Float.parseFloat(String.format("%.2f", time));
+				System.out.println("The Approximate percentage your device will be charged upto is: " + time + "%");
+				System.out.print("If you want to add negative offset, then add it in percentage: ");					
+				negOffset = (byte) Math.abs(scan.nextByte());				
+				time -= negOffset;				
+				System.out.println("The Approximate percentage your device will be charged upto, after negative offset is: " + time + "%");				
 			}
 			
-			//Calculating the Total Time in Minutes 
-			total = (( (float) (chargePerReq + offset) - currentChargePer) / charge15Min) * 15;								
-			total = Float.parseFloat(String.format("%.2f", total));			
 			
-			//Converting the total time into hours, minutes, and seconds 
-			hours = (int) total / 60;			
-			minutes = (int) total - (hours * 60);			
-			seconds = (int) ((total - (hours * 60) - minutes) * 60);								
-			System.out.println("The Approximate time required to charge the device is: " 
-							   + hours + ":" + minutes + "'" + seconds + "\"");
-						
 			//Checks if you want to continue the program
 			System.out.println("------------------------------------");
 			while (true) {
 				System.out.print("Do you wish to continue [y or n]: ");							
 				char repeat = scan.next().charAt(0);			
 				if (repeat == 'y' || repeat == 'Y') {	
-					System.out.print("\n");
-					scanReq = "";					
+					System.out.print("\n");					
+					calcMode = "";
+					scanReq = "";		
+					scan.nextLine();
 					break;
 				} else if (repeat == 'n' || repeat == 'N') {
 					System.out.println("The program has been terminated!");
@@ -116,8 +232,8 @@ public class ChargeTimeCalc {
 					System.exit(0);
 				} else {
 					System.out.println("\n+-------------WARNING!-------------+\n"
-									 + "| Please enter the given keyword!  |\n"
-									 + "+----------------------------------+\n");
+									   + "| Please enter the given keyword!  |\n"
+									   + "+----------------------------------+\n");
 				}
 			}
 		}
